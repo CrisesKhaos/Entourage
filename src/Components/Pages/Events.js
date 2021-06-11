9d4f4bc192d739ea2e9a57d54bff6dcd0424de78
Vedant <harshtewari6@gmail.com>
2021-06-11 10:14:33 +0530

    bruh

--- src/Components/Pages/Events.js
+++ src/Components/Pages/Events.js
@@ -12,16 +12,13 @@ function Events() {
         <div className="nancyDrew" onClick={() => setlogin(true)}>
           <div className="event-title">Fi-Nancy Drew</div>
           <div className="desc">
-            <p>
-              Be the Sherlock Holmes of your story and come up with modern
-              solutions for the ever-growing modern problems. But remember, you
-              have to be vigilant with the advent of technology; it has become
-              very easy to hide lies between the numbers. Everyone possesses the
-              quality of reading between the lines, only few smart ones can read
-              between the numbers, the prerequisite here, is Forensic
-              Accounting. If you are Fi-nancy enough then this is your reign to
-              claim.
-            </p>
+            Be the Sherlock Holmes of your story and come up with modern
+            solutions for the ever-growing modern problems. But remember, you
+            have to be vigilant with the advent of technology; it has become
+            very easy to hide lies between the numbers. Everyone possesses the
+            quality of reading between the lines, only few smart ones can read
+            between the numbers, the prerequisite here, is Forensic Accounting.
+            If you are Fi-nancy enough then this is your reign to claim.
           </div>
         </div>
 
